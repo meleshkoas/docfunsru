7d1cda63cade85edfe58acead6422fb33b33a751
Alexander Meleshko <a.meleshko@postgrespro.ru>
2025-09-17 04:07:32 +0300

    added list.bat

---
+++ list.bat
@@ -0,0 +1,37 @@
+:: 11241
+:: Kotova
+git clone https://github.com/Vas1lenko/textdoc1 11241/Kotova
+:: Kuzmin
+git clone https://github.com/fwefwal/htotototo/ 11241/Kuzmin
+:: Matveev
+git clone https://github.com/Egoref07/instruction 11241/Matveev
+:: Moshnikov
+git clone https://github.com/BYBLIK-C-MALINOY/First-tech-documentation.git 11241/Moshnikov
+:: Fedoseev
+git clone https://github.com/CeYn97/instructions 11241/Fedoseev
+:: Solotov
+git clone https://github.com/Ivan-Solotov/-.git 11241/Solotov
+:: 11242
+:: Alekseev
+git clone https://github.com/nikol99/- 11242/Alekseev
+:: Bokov
+git clone https://github.com/Tilrait/- 11242/Bokov
+:: Ivanov
+git clone https://github.com/MaysDaysYes/- 11242/Ivanov
+:: Larionova
+git clone https://github.com/deSpooky/CT1 11242/Larionova
+:: Ozerova
+git clone https://github.com/retawu/tech_docs_kt1-2 11242/Ozerova
+:: Parshin
+git clone https://github.com/Erokez0/tech-docs-basics 11242/Parshin
+::11245
+:: Alenchikova
+git clone https://github.com/Polina-mc21/- 11245/Alenchikova
+:: Potapenko
+git clone https://github.com/polinazenere/github.com-polina-zenere-lxp.txt 11245/Potapenko
+:: 11243
+:: Ryazanov
+git clone https://github.com/Ivan-Ryazanov/Creation_repository.git 11243/Ryazanov
+:: 11244
+:: Degtyareva
+git clone https://github.com/natahary/sectask/ 11244/Degtyareva
